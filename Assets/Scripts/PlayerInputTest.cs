249cbc7bca3f61c745c578d72aba891588f19b6c
thekuku92 <46631832+thekuku92@users.noreply.github.com>
2023-02-26 15:11:07 +0100

    Finished basic movement, added Animator to the Player

---
+++ Assets/Scripts/PlayerInputTest.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputTest : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+    public float collisionOffset = 0.05f;
+    public ContactFilter2D movementFilter;
+    Vector2 movementInput;
+    private Rigidbody2D rb;
+    List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
+
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate() {
+        if(movementInput != Vector2.zero) {
+            // Check for potential collisions
+            int count = rb.Cast(
+                movementInput, // X and Y values between -1 and 1 that represent the direction from the body to look for collisions
+                movementFilter, // The settings that determine where a collision can occur on such as layers to collide with
+                castCollisions, // List of collisions to store the found collisions into after the Cast is finished
+                moveSpeed * Time.fixedDeltaTime + collisionOffset); // The amount to cast equal to the movement plus an offset
+
+            rb.MovePosition(rb.position + movementInput * moveSpeed * Time.fixedDeltaTime);
+
+    }
+}
+    void OnMove(InputValue movementValue) {
+        movementInput = movementValue.Get<Vector2>();
+    }
+}
